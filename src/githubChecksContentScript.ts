f617503a56c178c1cea6b18825f5ce429d433638
Jan Jahoda <jajahoda@microsoft.com>
2021-06-30 14:12:11 +0200

    Add another workitem pattern

--- src/githubChecksContentScript.ts
+++ src/githubChecksContentScript.ts
@@ -2,8 +2,10 @@ import { attachReact } from "./HelixInfo"
 
 const main = () => {
 
-    const re = /Work item ([^\/]*)\/([^ ]*) in/
-
+    const regexes = [
+        /Failure log: https:\/\/helix.dot.net\/api\/(?<version>\d{4}-\d\d-\d\d)\/jobs\/(?<job>[^\/]*)\/workitems\/(?<workItem>[^\/]*)\/console/,
+        /Work item ([^\/]*)\/([^ ]*) in/
+    ]
     const x = document.getElementsByTagName("pre")
 
     for (const element of x) {
@@ -11,11 +13,14 @@ const main = () => {
         let workItem: string | undefined = undefined;
         let apiVersion = "2019-06-17";
 
-        const regexResult = re.exec(element.innerText)
-
-        if (regexResult && regexResult.length === 3) {
-            workItem = regexResult[2];
-            job = regexResult[1];
+        for (const re of regexes) {
+            const result = re.exec(element.innerText)
+            if (result?.groups) {
+                apiVersion = result.groups.version ?? apiVersion;
+                job = result.groups.job;
+                workItem = result.groups.workItem;
+                break;
+            }
         }
 
         if (job && workItem) {
